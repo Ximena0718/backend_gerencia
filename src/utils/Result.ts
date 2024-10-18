0cfa3b22988bd2979513d2f01a3d6abfb7a09dc0
Juanito-boop <acevedojuanesteban.colombia@gmail.com>
2024-10-18 00:15:07 -0500

    Agregar utilidades de caché y resultados Agrega las utilidades de caché y resultados al proyecto.

---
+++ src/utils/Result.ts
@@ -0,0 +1,43 @@
+class Result<T, E = string> {
+	public isSuccess: boolean;
+	public isFailure: boolean;
+	public error: E | null;
+	private value: T | null;
+
+	private constructor(isSuccess: boolean, error?: E, value?: T) {
+		if (isSuccess && error) {
+			throw new Error("A result cannot be successful and contain an error");
+		}
+		if (!isSuccess && !error) {
+			throw new Error("A failing result needs to contain an error message");
+		}
+
+		this.isSuccess = isSuccess;
+		this.isFailure = !isSuccess;
+		this.error = error || null;
+		this.value = value || null;
+	}
+
+	public getValue(): T {
+		if (!this.isSuccess) {
+			throw new Error(
+				"Can't get the value of an error result. Use 'errorValue' instead."
+			);
+		}
+		return this.value as T;
+	}
+
+	public errorValue(): E {
+		return this.error as E;
+	}
+
+	public static success<U>(value?: U): Result<U> {
+		return new Result<U>(true, undefined, value);
+	}
+
+	public static fail<U, F>(error: F): Result<U, F> {
+		return new Result<U, F>(false, error);
+	}
+}
+
+export default Result;
