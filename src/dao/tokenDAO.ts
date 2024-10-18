3c218f32be0e487b6d08cbae7d41be830bf65be9
Juanito-boop <acevedojuanesteban.colombia@gmail.com>
2024-10-18 00:14:29 -0500

    Agregar DAO de tokens y usuarios

---
+++ src/dao/tokenDAO.ts
@@ -0,0 +1,44 @@
+import { config } from "dotenv";
+import Jwt from "jsonwebtoken";
+import pool from "../config/connection/conexion";
+import { DataToken, Token } from "../interface/interfaces";
+import { SQL_TOKEN } from "../repository/crudSQL";
+import Result from "../utils/Result";
+
+config({ path: "./.env" });
+
+export default class tokenDAO {
+	public static async generateToken(data: Token[]): Promise<Result<string>> {
+		try {
+			const result = await pool.result(SQL_TOKEN.getUserToken, data);
+
+			if (result.rows.length === 0) {
+				return Result.fail("No se encontraron registros");
+			}
+
+			const { username, rol } = result.rows[0] as DataToken;
+			const secretKey = process.env.JWT_SECRET_KEY || "LaSuperClave";
+
+			const token = Jwt.sign({ username, rol }, secretKey, {
+				expiresIn: "1d",
+			});
+
+			return Result.success(token);
+		} catch (error: any) {
+			return Result.fail(`No se puede generar el token, ${error.message}`);
+		}
+	}
+
+	public static async getUserCredentials(
+		username: string
+	): Promise<Result<{ password: string }>> {
+		try {
+			const result = await pool.one(SQL_TOKEN.getUserCredentials, [username]);
+			return Result.success(result);
+		} catch (error: any) {
+			return Result.fail(
+				`No se puede obtener las credenciales del usuario: ${error.message}`
+			);
+		}
+	}
+}
