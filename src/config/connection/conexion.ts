542fc03690c6eabcfb1cbfb6a7dd6a44ce9c6297
Juanito-boop <acevedojuanesteban.colombia@gmail.com>
2024-10-18 00:13:45 -0500

    Agregar archivos de configuración y conexión a la base de datos

---
+++ src/config/connection/conexion.ts
@@ -0,0 +1,22 @@
+import pgPromise from "pg-promise";
+import { opcionesPG } from "./opcionConexion";
+import variablesConexion from "../domain/varDB";
+import Result from "../../utils/Result";
+
+const pgp = pgPromise(opcionesPG);
+const pool = pgp(variablesConexion);
+const dbname = variablesConexion.database;
+
+pool
+	.connect()
+	.then((conn) => {
+		console.log("Conexion exitosa con DB: ", dbname);
+		conn.done();
+	})
+	.catch((error) => {
+		return Result.fail(
+			`Error al conectar con la base de datos: ${error.message}`
+		);
+	});
+
+export default pool;
